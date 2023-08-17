8b2e8e2911a71fe46185846f6017df795451793b
Coday-meric <contact@aymeric-mai.fr>
2023-08-17 09:42:17 +0200

    ex01 and ex02

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -13,6 +13,14 @@ Fixed::~Fixed(){
 	std::cout << "Destructor called" << std::endl;
 }
 
+Fixed::Fixed( const int n ) : _vnvf( n << _bpf ) {
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed( const float n ) : _vnvf( std::roundf( n * ( 1 << _bpf ) ) ) {
+	std::cout << "Float constructor called" << std::endl;
+}
+
 Fixed::Fixed(Fixed const & src){
 	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
@@ -27,10 +35,22 @@ Fixed & Fixed::operator=(const Fixed &rhs) {
 }
 
 int Fixed::getRawBits( void ) const {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_vnvf);
 }
 
 void Fixed::setRawBits( int const raw ){
 	this->_vnvf = raw;
+}
+
+float   Fixed::toFloat( void ) const {
+	return static_cast<float>( this->getRawBits() ) / ( 1 << _bpf );
+}
+
+int     Fixed::toInt( void ) const {
+	return this->_vnvf >> _bpf;
+}
+
+std::ostream & operator<<( std::ostream & o, Fixed const & i ) {
+	o << i.toFloat();
+	return o;
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -5,20 +5,25 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 #include <iostream>
-
+#include <cmath>
 
 class Fixed {
 public:
 	Fixed(void);
 	~Fixed();
+	Fixed( const int n );
+	Fixed( const float n );
 	Fixed(Fixed const & src);
 	Fixed & operator=(Fixed const & rhs);
 	int getRawBits( void ) const;
 	void setRawBits( int const raw );
+	float   toFloat( void ) const;
+	int     toInt( void ) const;
 private:
 	int _vnvf;
 	static const int _bpf = 8;
 };
 
+std::ostream & operator<<( std::ostream & o, Fixed const & i );
 
 #endif //FIXED_HPP
